daf494a62443c212b7d09dcad6cce378de4f5641
jukJuchok <juk.juchok@yandex.ru>
2022-11-13 21:27:42 +0300

    inital commit

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import Chart from "./Chart.svelte";
+
+  let levels;
+
+  fill();
+
+  setInterval(() => {
+    clear();
+    setTimeout(() => {
+      fill();
+    }, 1000);
+  }, 2500);
+
+  function clear() {
+    levels = [0, 0, 0, 0, 0, 0, 0, 0];
+  }
+
+  function fill() {
+    levels = [
+      randomIntFromInterval(0, 6),
+      randomIntFromInterval(0, 6),
+      randomIntFromInterval(0, 6),
+      randomIntFromInterval(0, 6),
+      randomIntFromInterval(0, 6),
+      randomIntFromInterval(0, 6),
+      randomIntFromInterval(0, 6),
+      randomIntFromInterval(0, 6),
+    ];
+  }
+
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+</script>
+
+<Chart bind:levels />
